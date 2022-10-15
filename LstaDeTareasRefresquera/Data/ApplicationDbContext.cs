e8476fe32530c1cef5fe74fcb1bb5da368e56e89
luisraulag <61611501+luisraulag@users.noreply.github.com>
2022-10-15 11:49:01 -0500

    Add files via upload

---
+++ LstaDeTareasRefresquera/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ListaDeTareasRefresquera.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTareasRefresquera.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }  
+        public DbSet<Tarea> Tareas { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ LstaDeTareasRefresquera/Data/Migrations/20221014202857_AgregarTablaTarea.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaDeTareasRefresquera.Data.Migrations
+{
+    public partial class AgregarTablaTarea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    IdTarea = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreTarea = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaTarea = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstatusTarea = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.IdTarea);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
